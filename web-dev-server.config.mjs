850415500175cfb33cca11935d021741a813ebc8
aelbore <aelbore.sg@gmail.com>
2021-04-25 21:03:49 +0800

    fix the error

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -4,10 +4,10 @@ import { esbuildPlugin } from '@web/dev-server-esbuild'
 export default {
   nodeResolve: true,
   plugins: [
+    esbuildPlugin({ ts: true }),
     hmrPlugin({
       include: ['src/**/*'],
       presets: [presets.litElement],
-    }),
-    esbuildPlugin({ ts: true })
+    })
   ],
 };
