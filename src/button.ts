9f9512b84d9e36164c65a6cc8ca9637b478f9107
aelbore <aelbore.sg@gmail.com>
2021-04-27 22:01:39 +0800

    add rollup-plugin-lit-css for external css

--- src/button.ts
+++ src/button.ts
@@ -1,4 +1,5 @@
-import { LitElement, html, css } from 'lit-element'
+import { LitElement, html } from 'lit-element'
+import style from './button.css'
 
 export class Button extends LitElement {
 
@@ -13,38 +14,7 @@ export class Button extends LitElement {
   }
 
   static get styles() {
-    return css `
-      :host {
-				--bto-button-font-size-small: 12px;
-				--bto-button-font-size-medium: 14px;
-				--bto-button-font-size-large: 16px;
-      }
-
-			.btn {
-				font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-				font-weight: 700;
-				border: 0;
-				border-radius: 3em;
-				cursor: pointer;
-				display: inline-block;
-				line-height: 1;				
-			}
-
-			.button-small {
-				font-size: var(--bto-button-font-size-small);
-				padding: 10px 16px;
-			}
-
-			.button-medium {
-				font-size: var(--bto-button-font-size-meduim);
-				padding: 11px 20px;
-			}
-
-			.button-large {
-				font-size: var(--bto-button-font-size-large);
-				padding: 12px 24px;
-			}      
-    `
+    return [ style ]
   }
 
   render() {
--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,9 +1,16 @@
 import { hmrPlugin, presets } from '@open-wc/dev-server-hmr'
 import { esbuildPlugin } from '@web/dev-server-esbuild'
+import { fromRollup } from '@web/dev-server-rollup';
+
+import litcss from 'rollup-plugin-lit-css'
 
 export default {
   nodeResolve: true,
+  mimeTypes: {
+    '**/*.css': 'js',
+  },
   plugins: [
+    fromRollup(litcss)({ include: [ './src/**/*.css' ] }),
     esbuildPlugin({ ts: true }),
     hmrPlugin({
       include: ['src/**/*'],
